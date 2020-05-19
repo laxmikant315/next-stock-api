f17809d3a5604cae712cfce3da483e55e3432595
laxmikantphadke@gmail.com <laxmikantphadke@gmail.com>
2020-05-19 18:48:05 +0530

    ss

--- src/models/app-settings.ts
+++ src/models/app-settings.ts
@@ -2,8 +2,7 @@ import * as mongoose from 'mongoose';
 
 const appSettingsSchema = new mongoose.Schema({
     
-    nifty100Stocks: Array,
-    dailyVolitilityStocks:Array,
+    intradayStocks: Array
   
 })
 
